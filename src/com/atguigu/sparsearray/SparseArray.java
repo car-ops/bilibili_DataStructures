475d778292c672852508b85eb279a84ac91a9ef0
Xuner1213 <1531002208@qq.com>
2019-09-03 23:34:59 +0800

    修改稀疏数组

--- src/com/atguigu/sparsearray/SparseArray.java
+++ src/com/atguigu/sparsearray/SparseArray.java
@@ -5,6 +5,7 @@ public class SparseArray {
         int chessArr1[][] = new int[11][11];
         chessArr1[1][2] = 1;
         chessArr1[2][3] = 2;
+        chessArr1[4][5] = 2;
         System.out.println("原始的二维数组");
         for (int[] row: chessArr1) {
             for (int data: row) {
@@ -43,6 +44,22 @@ public class SparseArray {
         System.out.println();
         System.out.println("得到的稀疏数组为：");
         for (int[] row: sparseArr) {
+            for (int data: row) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+
+
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+        for (int[] row: chessArr2) {
             for (int data: row) {
                 System.out.printf("%d\t", data);
             }
