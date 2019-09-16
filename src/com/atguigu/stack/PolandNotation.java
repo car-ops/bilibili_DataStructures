99fea4ff8f85bc18261fc95b64386e924d455998
Xuner1213 <1531002208@qq.com>
2019-09-16 23:02:01 +0800

    中缀表达式转后缀表达式

--- src/com/atguigu/stack/PolandNotation.java
+++ src/com/atguigu/stack/PolandNotation.java
@@ -6,16 +6,83 @@ import java.util.Stack;
 
 public class PolandNotation {
     public static void main(String[] args) {
+        String expression = "1+((2+3)*4)-5";
+        List<String> infixExpressionList = toInfixExpressionList(expression);
+        System.out.println(infixExpressionList);
+        List<String> parseSuffixExpressionList = parseSuffixExpressionList(infixExpressionList);
+        System.out.println(parseSuffixExpressionList);
+
         //先定义一个逆波兰表达式
         //说明为了方便，逆波兰表达式的数字和符号用空格隔开
-        String suffixExpression = "3 4 + 5 * 6 -";
-        //思路
-        //1.先将"3 4 + 5 * 6 -"放到ArrayList中
-        //2.将ArrayList传递给一个方法，遍历ArrayList配合栈完成计算
-        List<String> rpnList = getListString(suffixExpression);
-        System.out.println("rpnList=" + rpnList);
-        int res = calculate(rpnList);
-        System.out.println("计算的结果是=" + res);
+//        String suffixExpression = "30 4 + 5 * 6 -";
+//        //思路
+//        //1.先将"3 4 + 5 * 6 -"放到ArrayList中
+//        //2.将ArrayList传递给一个方法，遍历ArrayList配合栈完成计算
+//        List<String> rpnList = getListString(suffixExpression);
+//        System.out.println("rpnList=" + rpnList);
+//        int res = calculate(rpnList);
+//        System.out.println("计算的结果是=" + res);
+    }
+
+    //方法：将得到的中缀表达式对应的List => 后缀表达式对应的List
+    public static List<String> parseSuffixExpressionList(List<String> ls) {
+        //定义两个栈
+        Stack<String> s1 = new Stack<String>();//符号栈
+        //s2在整个转换过程中，没有pop操作，而且后面我们还有逆序输出
+        //直接使用List
+        //Stack<String> s2 = new Stack<String>();//存储中间结果的栈s2
+        List<String> s2 = new ArrayList<String>();
+
+        //遍历ls
+        for (String item : ls) {
+            if (item.matches("\\d+")) {
+                s2.add(item);
+            } else if (item.equals("(")) {
+                s1.push(item);
+            } else if (item.equals(")")) {
+                while (!s1.peek().equals("(")) {
+                    s2.add(s1.pop());
+                }
+                s1.pop();//将(弹出s1栈，消除小括号
+            } else {
+                //当item的优先级小于或等于s1，将s1栈顶的运算符弹出并压入到s2中
+                //我们缺少一个比较优先级高低的方法
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) >= Operation.getValue(item)) {
+                    s2.add(s1.pop());
+                }
+                //还需要将item压入栈
+                s1.push(item);
+            }
+        }
+        //将s1中剩余的运算符依次弹出并加入s2
+        while (s1.size() != 0) {
+            s2.add(s1.pop());
+        }
+        return s2;
+    }
+
+    //方法：将中缀表达式转成对应的List
+    public static List<String> toInfixExpressionList(String s) {
+        //定义一个List,存放中缀表达式对应的内容
+        List<String> ls = new ArrayList<String>();
+        int i = 0;//这是一个指针，用于遍历中缀表达式字符串
+        String str;//对多位数的拼接
+        char c;//每遍历一个字符，就放入c
+        do {
+            //如果c是一个非数字，我们就需要加入到ls
+            if ((c = s.charAt(i)) < 48 || (c = s.charAt(i)) > 57) {
+                ls.add("" + c);
+                i++;//i需要后移
+            } else {//如果是一个数，需要考虑多位数
+                str = "";//先将str置成""
+                while (i < s.length() && (c = s.charAt(i)) >= 48 && (c = s.charAt(i)) <= 57) {
+                    str += c;
+                    i++;
+                }
+                ls.add(str);
+            }
+        } while (i < s.length());
+        return ls;
     }
 
     //将一个逆波兰表达式，依次将数据和运算符放入到ArrayList中
@@ -61,5 +128,36 @@ public class PolandNotation {
         }
         //最后留在stack中的数据是运算结果
         return Integer.parseInt(stack.pop());
+    }
+}
+
+//编写一个类Operation可以返回一个运算符对应的优先级
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    //写一个方法，返回对应的优先级的数字
+    public static int getValue(String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = ADD;
+                break;
+            case "-":
+                result = SUB;
+                break;
+            case "*":
+                result = MUL;
+                break;
+            case "/":
+                result = DIV;
+                break;
+            default:
+                System.out.println("不存在该运算符");
+                break;
+        }
+        return result;
     }
 }
