da4602817560bbf17b794b636b299d3ebcd63066
Xuner1213 <1531002208@qq.com>
2019-10-04 12:14:26 +0800

    quick排序

--- src/com/atguigu/sort/QuickSort.java
+++ src/com/atguigu/sort/QuickSort.java
@@ -9,25 +9,26 @@ public class QuickSort {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void quickSort(int[] arr, int left, int right) {
-        int l = left;//左下标
-        int r = right;//右下标
-        //pivot中轴
+    public static void quickSort(int[] arr,int left, int right) {
+        int l = left; //左下标
+        int r = right; //右下标
+        //pivot 中轴值
         int pivot = arr[(left + right) / 2];
-        int temp = 0;
-        //while循环的目的是让比pivot小的放在左边
-        //比pivot大的放到右边
-        while (l < r) {
-            //在pivot的左边一直找，找到大于pivot值，才退出
-            while (arr[l] < pivot) {
+        int temp = 0; //临时变量，作为交换时使用
+        //while循环的目的是让比pivot 值小放到左边
+        //比pivot 值大放到右边
+        while( l < r) {
+            //在pivot的左边一直找,找到大于等于pivot值,才退出
+            while( arr[l] < pivot) {
                 l += 1;
             }
-            //在pivot的右边一直找，找到小于pivot的值，才退出
-            while (arr[r] > pivot) {
+            //在pivot的右边一直找,找到小于等于pivot值,才退出
+            while(arr[r] > pivot) {
                 r -= 1;
             }
-            //如果 l >= r,说明pivot左右两边的值，已经按照左边全部小于或等于pivot,右边大于等于pivot
-            if (l >= r) {
+            //如果l >= r说明pivot 的左右两的值，已经按照左边全部是
+            //小于等于pivot值，右边全部是大于等于pivot值
+            if( l >= r) {
                 break;
             }
 
@@ -35,26 +36,31 @@ public class QuickSort {
             temp = arr[l];
             arr[l] = arr[r];
             arr[r] = temp;
-            //如果交换完之后，发现这个arr[l] == pivot，值相等r--，前移
-            if (arr[l] == pivot) {
-                r -= 1;
-            }
-            if (arr[r] == pivot) {
-                l += 1;
-            }
 
-            if (l == r) {
-                l += 1;
+            //如果交换完后，发现这个arr[l] == pivot值 相等 r--， 前移
+            if(arr[l] == pivot) {
                 r -= 1;
             }
-            //向左递归
-            if (left < r) {
-                quickSort(arr, left, r);
-            }
-            //向右递归
-            if (right > l) {
-                quickSort(arr, l, right);
+            //如果交换完后，发现这个arr[r] == pivot值 相等 l++， 后移
+            if(arr[r] == pivot) {
+                l += 1;
             }
         }
+
+        // 如果 l == r, 必须l++, r--, 否则为出现栈溢出
+        if (l == r) {
+            l += 1;
+            r -= 1;
+        }
+        //向左递归
+        if(left < r) {
+            quickSort(arr, left, r);
+        }
+        //向右递归
+        if(right > l) {
+            quickSort(arr, l, right);
+        }
+
+
     }
 }
