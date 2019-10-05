28976bd060617013520772b82fb3792213aadbc6
Xuner1213 <1531002208@qq.com>
2019-10-05 09:47:40 +0800

    归并排序

--- src/com/atguigu/sort/MergeSort.java
+++ src/com/atguigu/sort/MergeSort.java
@@ -11,64 +11,81 @@ public class MergeSort {
     }
 
     //分+合
+    //分+合方法
     public static void mergeSort(int[] arr, int left, int right, int[] temp) {
-        if (left < right) {
-            int mid = (left + right) / 2;
+        if(left < right) {
+            int mid = (left + right) / 2; //中间索引
+            //向左递归进行分解
             mergeSort(arr, left, mid, temp);
+            //向右递归进行分解
             mergeSort(arr, mid + 1, right, temp);
             //合并
             merge(arr, left, mid, right, temp);
+
         }
     }
 
+    //合并的方法
     /**
+     *
      * @param arr 排序的原始数组
      * @param left 左边有序序列的初始索引
-     * @param right 右边索引
      * @param mid 中间索引
-     * @param temp 中转数组
+     * @param right 右边索引
+     * @param temp 做中转的数组
      */
-    //合并
-    public static void merge(int[] arr, int left, int right, int mid, int[] temp) {
-        int i = left;//初始化i,左边有序序列的初始值
-        int j = mid + 1;//初始化j,右边有序序列的初始索引
-        int t = 0;//指向temp当前索引
-        //先把左右2边（有序）的数据按规则填充到temp数组
-        //直到左右两边的有序序列，有一边处理完为止
+    public static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+
+        int i = left; // 初始化i, 左边有序序列的初始索引
+        int j = mid + 1; //初始化j, 右边有序序列的初始索引
+        int t = 0; // 指向temp数组的当前索引
+
+        //(一)
+        //先把左右两边(有序)的数据按照规则填充到temp数组
+        //直到左右两边的有序序列，有一边处理完毕为止
         while (i <= mid && j <= right) {//继续
-            //如果左边的有序序列的当前元素，小于等于有序序列的当前元素
-            //即将左边的当前元素，拷贝到temp数组中去
-            //然后t++,i++
-            if (arr[i] <= arr[j]) {
+            //如果左边的有序序列的当前元素，小于等于右边有序序列的当前元素
+            //即将左边的当前元素，填充到 temp数组
+            //然后 t++, i++
+            if(arr[i] <= arr[j]) {
                 temp[t] = arr[i];
                 t += 1;
                 i += 1;
-            } else {
+            } else { //反之,将右边有序序列的当前元素，填充到temp数组
                 temp[t] = arr[j];
                 t += 1;
                 j += 1;
             }
-
         }
 
-        //把剩余数据的一边数据一次填充到temp
-        while (i <= mid) {
+        //(二)
+        //把有剩余数据的一边的数据依次全部填充到temp
+        while( i <= mid) { //左边的有序序列还有剩余的元素，就全部填充到temp
             temp[t] = arr[i];
             t += 1;
             i += 1;
         }
-        while (j <= right) {
+
+        while( j <= right) { //右边的有序序列还有剩余的元素，就全部填充到temp
             temp[t] = arr[j];
             t += 1;
             j += 1;
         }
-        //将temp数组拷贝到arr
+
+
+        //(三)
+        //将temp数组的元素拷贝到arr
+        //注意，并不是每次都拷贝所有
         t = 0;
-        int tempLeft = left;
-        while (tempLeft <= right) {
+        int tempLeft = left; //
+        //第一次合并 tempLeft = 0 , right = 1 //  tempLeft = 2  right = 3 // tL=0 ri=3
+        //最后一次 tempLeft = 0  right = 7
+        while(tempLeft <= right) {
             arr[tempLeft] = temp[t];
             t += 1;
             tempLeft += 1;
         }
+
     }
+
 }
